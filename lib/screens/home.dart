003a37392245b2dc473566e9996c4439422b6e98
ahmed <atef87133@gmail.com>
2025-04-15 03:29:21 +0200

    w500

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,7 +15,7 @@ class home extends StatelessWidget {
           style: TextStyle(
             fontSize: 24,
             color: Colors.black,
-            fontWeight: FontWeight.w600,
+            fontWeight: FontWeight.w500,
           ),
         ),
       ),
